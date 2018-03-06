5e38582d1d77a40a7424e1dfc351c684b6bbe2d2
pda00 <36547803+pda00@users.noreply.github.com>
2018-03-06 16:24:33 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+var v='0';
+self.addEventListener('install',function(event){
+  event.waitUntil(
+    caches.open(v).then(function(cache){
+      return cache.add('/sw/');
+    })
+  )
+});
+self.addEventListener('activate',function(event){
+  event.waitUntil(
+    caches.keys().then(function(keyList){
+      return Promise.all(keyList.map(function(key,i){
+        if(key!==v)return caches.delete(keyList[i])
+      }))
+    })
+  )
+});
+self.addEventListener('fetch',function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response) return response;
+      return fetch(event.request);
+    })
+  )
+});
